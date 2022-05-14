835266f697603aff4c917efdc18f6e5a487ffbe8
Francisco/LyNux'nt <74838685+ffuentealba19@users.noreply.github.com>
2022-05-14 12:03:25 -0400

    inicio
    todo con lo que se comenzó

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import Sidebar from './components/Sidebar/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+        <div>texto de prueba</div>
+        <div className='Sidebar'>
+          <Sidebar></Sidebar>
+        </div>
+    </div>
+  );
+}
+
+export default App;
