9614f5b26a9e493b75139c89e5c46281b509ed69
lortegacurillan <lfjoc123@gmail.com>
2022-06-06 22:59:42 -0400

    Calculo IMC con base de datos
    Calculo del indice de masa corporal  conectada  a la base de datos con React

---
+++ IMC/server/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "",
+  database: "informacion",
+});
+
+app.post("/agregar", (req, res) => {
+  const estatura = req.body.estatura;
+  const peso = req.body.peso;
+  const imc = (peso/(estatura)**2);
+  let f = new Date();
+  let fecha=(f.getFullYear() + "." + (f.getMonth() +1) + "." +f.getDate());
+  db.query(
+    "INSERT INTO prueba (estatura, peso, fecha, imc) VALUES (?,?,?,?)",
+    [estatura, peso, fecha, imc],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Valores Ingresados");
+      }
+    }
+  );
+});
+
+app.get("/registros", (req, res) => {
+  db.query("SELECT estatura,peso,SUBSTRING(fecha,1,10) As fecha,imc FROM prueba " , (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.delete("/borrar/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("DELETE FROM prueba WHERE id = ?", id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log("Servidor funcionando en el puerto 3001!");
+});
